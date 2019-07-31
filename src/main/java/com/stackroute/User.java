1f608423c3bdd2c752173d4a625f8f370a40f2e0
adithyaGowda <adithyagowda5696@gmail.com>
2019-07-31 07:46:00 +0530

    Spring mvc task-1

---
+++ src/main/java/com/stackroute/User.java
@@ -0,0 +1,14 @@
+package com.stackroute;
+
+public class User {
+
+    String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
